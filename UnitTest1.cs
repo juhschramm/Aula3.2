df61d025f210239a618e29112145a6bf6d0d19c8
Igor Ramos <30807333+IgortRamos@users.noreply.github.com>
2019-09-07 16:37:03 -0300

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,7 +8,58 @@ namespace XUnitTestProject1
         [Fact]
         public void Test1()
         {
+            var fiesta = new CarroDeIgnicaoViaBotao
+            {
+                Cor = "preto"
+            };
 
+            var logan = new CarroDeIgnicaoViaChave()
+            {
+                Cor = "branco"
+            };
+
+            var superBarco = new SuperBarco()
+            {
+                Cor = "azul",
+                EspessuraDoCasco = 2
+            };
+
+
+            Desmontar(fiesta);
+            Desmontar(logan);
+
+            Desmontar(superBarco);
+        }
+
+        public void Desmontar(Carro carro)
+        {
+        }
+
+        public void Desmontar(IBarcoMotor barcoMotor)
+        {
+        }
+
+        public void Desmontar(IVeiculo veiculo)
+        {
+        }
+
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 1, 2)]
+        [InlineData(0, 0, 0)]
+        public void Soma_deve__somar_corretamentes(int num1, int num2, int expected)
+        {
+            var actual = new Matematica().Soma(num1, num2);
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class Matematica
+    {
+        public int Soma(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
